f975d6a027e9bbca5c20fb745a56d41ee93010fc
edulanarf <edulanarfrgmail.com>
2024-11-27 13:34:06 +0000

    Se comienza con la creacion de las clases swing. Se crea la clase SwingCurrencyDialog en donde se quiere mostrar una lista desplegable de las diferentes divisas

---
+++ src/main/java/software/ulpgc/moneycalculatorv2/apps/windows/view/SwingCurrencyDialog.java
@@ -0,0 +1,30 @@
+package software.ulpgc.moneycalculatorv2.apps.windows.view;
+
+import software.ulpgc.moneycalculatorv2.architecture.model.Currency;
+import software.ulpgc.moneycalculatorv2.architecture.view.CurrencyDialog;
+
+import javax.swing.*;
+import java.util.List;
+
+public class SwingCurrencyDialog extends JPanel implements CurrencyDialog {
+    private final List<Currency> currencies;
+    private final JComboBox<Currency>selector;
+
+    public SwingCurrencyDialog(List<Currency> currencies) {
+        this.currencies = currencies;
+        this.add(selector = createSelector());
+    }
+
+    private JComboBox<Currency> createSelector() {//Lista desplegable que mostrara todos los tipos de currencies
+        JComboBox<Currency> comboBox = new JComboBox<>();
+        for(Currency currency: currencies){
+            comboBox.addItem(currency);
+        }
+        return comboBox;
+    }
+
+    @Override
+    public Currency get() {
+        return null;
+    }
+}
